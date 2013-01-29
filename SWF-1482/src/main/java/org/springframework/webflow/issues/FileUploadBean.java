31f3e799a33f47211756d4394844dd096cd4a68b
Phillip Webb <pwebb@vmware.com>
2013-01-29 15:02:33 -0800

    Prevent redirect and render a response

--- SWF-1482/src/main/java/org/springframework/webflow/issues/FileUploadBean.java
+++ SWF-1482/src/main/java/org/springframework/webflow/issues/FileUploadBean.java
@@ -2,16 +2,22 @@ package org.springframework.webflow.issues;
 
 import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+
 import org.richfaces.event.FileUploadEvent;
 import org.richfaces.model.UploadedFile;
+import org.springframework.webflow.context.ExternalContextHolder;
 
 public class FileUploadBean implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public void listener(FileUploadEvent event) throws Exception{
         UploadedFile file = event.getUploadedFile();
+        FacesContext.getCurrentInstance().getPartialViewContext().processPartial(PhaseId.RENDER_RESPONSE);
+        ExternalContextHolder.getExternalContext().recordResponseComplete();
         System.out.println("Uploaded file: " + file.getName());
     }
-	
+
 }
